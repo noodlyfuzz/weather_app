eb42e531135fbcaa9f979e8118d3653acf03b90f
user <useruseruseruseruserabcdefg@mail.ru>
2022-02-17 01:20:56 +0300

    Made title depends on BottomNavigationBar.

--- lib/MainScreen/main_screen.dart
+++ lib/MainScreen/main_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/MainScreen/geolocation.dart';
 import 'package:weather_app/ForecastWindow/forecast_widget.dart';
 import 'package:weather_app/TodaysWindow/today_weather_widget.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 class MainScreenWidget extends StatefulWidget {
   const MainScreenWidget({Key? key}) : super(key: key);
@@ -12,12 +14,70 @@ class MainScreenWidget extends StatefulWidget {
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
   int _selectedTab = 0;
+  String _title = 'Weather here';
+  String? forecastLocation = '';
+
+  @override
+  initState() {
+    _title = 'Today';
+    getLocation();
+  }
 
   void onSelectedTab(int index) {
     if (_selectedTab == index) return;
+
+    switch (index) {
+      case 0:
+        {
+          _title = 'Today';
+        }
+        break;
+      case 1:
+        {
+          _title = forecastLocation!;
+        }
+        break;
+    }
+
     setState(() {
       _selectedTab = index;
     });
+  }
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  void getLocation() async {
+    Position position = await _getGeoLocationPosition();
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    forecastLocation = '${place.locality}';
   }
 
   @override
@@ -32,18 +92,16 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
             BottomNavigationBarItem(icon: Icon(Icons.today), label: 'today'),
             BottomNavigationBarItem(
                 icon: Icon(Icons.cloud_circle_sharp), label: 'forecast'),
-            // BottomNavigationBarItem(icon: Icon(Icons.today), label: ':D'),
           ],
           onTap: onSelectedTab,
         ),
         appBar: AppBar(
-          title: Text('Weather app'),
+          title: Text(_title),
           centerTitle: true,
         ),
         body: IndexedStack(
           index: _selectedTab,
           children: [
-            // GeolocationWidget(),
             TodayWeatherWidget(),
             ForecastWidget(),
           ],
